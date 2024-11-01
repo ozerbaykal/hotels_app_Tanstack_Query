6c5ef0904b8c079ed958a628e5d5cf30f26e1e86
ozerbaykal <baykalozer87@gmail.com>
2024-11-01 18:19:37 +0300

    tanstack query added and set

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+//client örneği oluştur
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
+
+
   <React.StrictMode>
-    <App />
+    {/* sağlayıcı ile app bileşenini sarmala */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+
+    </QueryClientProvider>
+
   </React.StrictMode>,
 )
